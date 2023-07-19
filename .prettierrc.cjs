91d217d4f8a909b047348d0fcad4123b249abe42
Jack Witt <Jwitt1452@gmail.com>
2023-07-19 12:30:22 -0700

    chore: add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+// @ts-check
+/// <reference types="@prettier/plugin-pug/src/prettier" />
+
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+    // plugins: [require.resolve('@prettier/plugin-pug')],
+
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+
+    // pugSingleQuote: false,
+}
--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,36 @@
-import express from "express";
-import path from "path";
+import express from 'express'
+import path from 'path'
 
 const todoList = [
-  {
-    title: "Groceries",
-    dueDate: "Today",
-  },
-  {
-    title: "Clean Car",
-    dueDate: "Yesterday",
-  },
-  {
-    title: "Submit Taxes",
-    dueDate: "08/27/2001",
-  },
-];
+    {
+        title: 'Groceries',
+        dueDate: 'Today',
+    },
+    {
+        title: 'Clean Car',
+        dueDate: 'Yesterday',
+    },
+    {
+        title: 'Submit Taxes',
+        dueDate: '08/27/2001',
+    },
+]
 
-const app = express();
+const app = express()
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 
-app.get("/", function (req, res) {
-  res.render("index", {
-    name: "Jack",
-  });
-});
+app.get('/', function (req, res) {
+    res.render('index', {
+        name: 'Jack',
+    })
+})
 
-app.get("/todos", function (req, res) {
-  res.render("components/_todo-list", { list: todoList });
-});
+app.get('/todos', function (req, res) {
+    res.render('components/_todo-list', { list: todoList })
+})
 
 app.listen(3000, () => {
-  console.log("server is running!");
-});
+    console.log('server is running!')
+})
