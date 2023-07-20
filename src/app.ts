99a806adb48213e20b61de70e40e06f1de928f76
Jack Witt <Jwitt1452@gmail.com>
2023-07-20 15:41:52 -0700

    fix: add completed and refresh controls

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,25 @@
 import express from 'express'
 import path from 'path'
+import 'dotenv/config'
 
 const todoList = [
     {
         id: '1',
         title: 'Groceries',
         dueDate: 'Today',
+        completed: false,
     },
     {
         id: '2',
         title: 'Clean Car',
         dueDate: 'Yesterday',
+        completed: false,
     },
     {
         id: '3',
         title: 'Submit Taxes',
         dueDate: '08/27/2001',
+        completed: false,
     },
 ]
 
@@ -24,6 +28,13 @@ const app = express()
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
+if (process.env.ENABLE_DEVELOPMENT_LOGGING === 'true') {
+    app.use((req, res, next) => {
+        req.on('end', () => console.log(todoList))
+        next()
+    })
+}
+
 app.get('/', function (req, res) {
     res.render('index', {
         name: 'Jack',
@@ -31,7 +42,24 @@ app.get('/', function (req, res) {
 })
 
 app.get('/todos', function (req, res) {
-    res.render('components/_todo-list', { list: todoList })
+    res.render('components/_todo-list', {
+        list: todoList.filter((todo) => !todo.completed),
+    })
+})
+
+app.get('/todos/completed', function (req, res) {
+    res.render('components/_todo-list', {
+        list: todoList.filter((todo) => todo.completed),
+    })
+})
+
+app.post('/todos/:id/toggle', function ({ params }, res) {
+    const { id } = params
+    const todo = todoList.find((todo) => todo.id === id)
+    if (todo) {
+        todo.completed = !todo.completed
+    }
+    res.render('wrappers/_todo-wrapper', { ...todo })
 })
 
 app.delete('/todos/:id', function ({ params }, res) {
@@ -40,7 +68,7 @@ app.delete('/todos/:id', function ({ params }, res) {
     if (todo) {
         todoList.splice(todoList.indexOf(todo), 1)
     }
-    res.render('components/_todo-list', { list: todoList })
+    res.render('wrappers/_todo-wrapper', { id: '-1' })
 })
 
 app.listen(3000, () => {
